f458b63c1abf16527c801fa30fcd0e74c2b8ea8c
elvino <elvino_tan@yahoo.com>
2019-05-30 15:05:45 +0700

    Testing Untuk Spring bus

--- src/main/java/com/elvino/ribbon/SpringRibbonRestClient.java
+++ src/main/java/com/elvino/ribbon/SpringRibbonRestClient.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
+@RefreshScope
 public class SpringRibbonRestClient {
 
 	@Autowired
